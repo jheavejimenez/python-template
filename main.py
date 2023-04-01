f36f18437725f0af159aa93dda314278e983d90b
Jhv <jimenezjheave123@gmail.com>
2023-04-01 16:55:31 +0800

    refactor

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # This is a sample Python script.
 
 # Press ⌃F5 to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
 def print_hi(name):
@@ -12,5 +11,3 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == "__main__":
     print_hi("PyCharm")
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
